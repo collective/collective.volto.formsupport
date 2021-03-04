ba989539129f7f69aa4cd45dd7dd92f34f2bc701
Andrea Cecchi <andrea.cecchi85@gmail.com>
2021-03-04 16:11:00 +0100

    fix tests and cleanup package

--- src/collective/volto/formsupport/testing.py
+++ src/collective/volto/formsupport/testing.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
 from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
-from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
 from plone.app.testing import (
     applyProfile,
     FunctionalTesting,
     IntegrationTesting,
     PloneSandboxLayer,
+    quickInstallProduct,
 )
+from plone.restapi.testing import PloneRestApiDXLayer
 from plone.testing import z2
 
+import collective.MockMailHost
 import collective.volto.formsupport
+import plone.restapi
 
 
-class CollectiveVoltoFormsupportLayer(PloneSandboxLayer):
+class VoltoFormsupportLayer(PloneSandboxLayer):
 
     defaultBases = (PLONE_APP_CONTENTTYPES_FIXTURE,)
 
@@ -21,33 +24,55 @@ class CollectiveVoltoFormsupportLayer(PloneSandboxLayer):
         # The z3c.autoinclude feature is disabled in the Plone fixture base
         # layer.
         import plone.restapi
+
         self.loadZCML(package=plone.restapi)
         self.loadZCML(package=collective.volto.formsupport)
 
     def setUpPloneSite(self, portal):
-        applyProfile(portal, 'collective.volto.formsupport:default')
+        applyProfile(portal, "collective.volto.formsupport:default")
 
 
-COLLECTIVE_VOLTO_FORMSUPPORT_FIXTURE = CollectiveVoltoFormsupportLayer()
+VOLTO_FORMSUPPORT_FIXTURE = VoltoFormsupportLayer()
 
 
-COLLECTIVE_VOLTO_FORMSUPPORT_INTEGRATION_TESTING = IntegrationTesting(
-    bases=(COLLECTIVE_VOLTO_FORMSUPPORT_FIXTURE,),
-    name='CollectiveVoltoFormsupportLayer:IntegrationTesting',
+VOLTO_FORMSUPPORT_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(VOLTO_FORMSUPPORT_FIXTURE,),
+    name="VoltoFormsupportLayer:IntegrationTesting",
 )
 
 
-COLLECTIVE_VOLTO_FORMSUPPORT_FUNCTIONAL_TESTING = FunctionalTesting(
-    bases=(COLLECTIVE_VOLTO_FORMSUPPORT_FIXTURE,),
-    name='CollectiveVoltoFormsupportLayer:FunctionalTesting',
+VOLTO_FORMSUPPORT_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(VOLTO_FORMSUPPORT_FIXTURE,),
+    name="VoltoFormsupportLayer:FunctionalTesting",
 )
 
 
-COLLECTIVE_VOLTO_FORMSUPPORT_ACCEPTANCE_TESTING = FunctionalTesting(
-    bases=(
-        COLLECTIVE_VOLTO_FORMSUPPORT_FIXTURE,
-        REMOTE_LIBRARY_BUNDLE_FIXTURE,
-        z2.ZSERVER_FIXTURE,
-    ),
-    name='CollectiveVoltoFormsupportLayer:AcceptanceTesting',
+class VoltoFormsupportRestApiLayer(PloneRestApiDXLayer):
+
+    defaultBases = (PLONE_APP_CONTENTTYPES_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        super(VoltoFormsupportRestApiLayer, self).setUpZope(
+            app, configurationContext
+        )
+        self.loadZCML(package=collective.MockMailHost)
+        self.loadZCML(package=plone.restapi)
+        self.loadZCML(package=collective.volto.formsupport)
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, "collective.volto.formsupport:default")
+        applyProfile(portal, "plone.restapi:blocks")
+        quickInstallProduct(portal, "collective.MockMailHost")
+        applyProfile(portal, "collective.MockMailHost:default")
+
+
+VOLTO_FORMSUPPORT_API_FIXTURE = VoltoFormsupportRestApiLayer()
+VOLTO_FORMSUPPORT_API_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(VOLTO_FORMSUPPORT_API_FIXTURE,),
+    name="VoltoFormsupportRestApiLayer:Integration",
+)
+
+VOLTO_FORMSUPPORT_API_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(VOLTO_FORMSUPPORT_API_FIXTURE, z2.ZSERVER_FIXTURE),
+    name="VoltoFormsupportRestApiLayer:Functional",
 )
--- src/collective/volto/formsupport/tests/test_setup.py
+++ src/collective/volto/formsupport/tests/test_setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Setup tests for this package."""
 from collective.volto.formsupport.testing import (
-    COLLECTIVE_VOLTO_FORMSUPPORT_INTEGRATION_TESTING  # noqa: E501,
+    VOLTO_FORMSUPPORT_INTEGRATION_TESTING,  # noqa: E501,
 )
 from plone import api
 from plone.app.testing import setRoles, TEST_USER_ID
@@ -18,56 +18,62 @@ except ImportError:
 class TestSetup(unittest.TestCase):
     """Test that collective.volto.formsupport is properly installed."""
 
-    layer = COLLECTIVE_VOLTO_FORMSUPPORT_INTEGRATION_TESTING
+    layer = VOLTO_FORMSUPPORT_INTEGRATION_TESTING
 
     def setUp(self):
         """Custom shared utility setup for tests."""
-        self.portal = self.layer['portal']
+        self.portal = self.layer["portal"]
         if get_installer:
-            self.installer = get_installer(self.portal, self.layer['request'])
+            self.installer = get_installer(self.portal, self.layer["request"])
         else:
-            self.installer = api.portal.get_tool('portal_quickinstaller')
+            self.installer = api.portal.get_tool("portal_quickinstaller")
 
     def test_product_installed(self):
         """Test if collective.volto.formsupport is installed."""
-        self.assertTrue(self.installer.isProductInstalled(
-            'collective.volto.formsupport'))
+        self.assertTrue(
+            self.installer.isProductInstalled("collective.volto.formsupport")
+        )
 
     def test_browserlayer(self):
         """Test that ICollectiveVoltoFormsupportLayer is registered."""
         from collective.volto.formsupport.interfaces import (
-            ICollectiveVoltoFormsupportLayer)
-        from plone.browserlayer import utils
-        self.assertIn(
             ICollectiveVoltoFormsupportLayer,
-            utils.registered_layers())
+        )
+        from plone.browserlayer import utils
+
+        self.assertIn(
+            ICollectiveVoltoFormsupportLayer, utils.registered_layers()
+        )
 
 
 class TestUninstall(unittest.TestCase):
 
-    layer = COLLECTIVE_VOLTO_FORMSUPPORT_INTEGRATION_TESTING
+    layer = VOLTO_FORMSUPPORT_INTEGRATION_TESTING
 
     def setUp(self):
-        self.portal = self.layer['portal']
+        self.portal = self.layer["portal"]
         if get_installer:
-            self.installer = get_installer(self.portal, self.layer['request'])
+            self.installer = get_installer(self.portal, self.layer["request"])
         else:
-            self.installer = api.portal.get_tool('portal_quickinstaller')
+            self.installer = api.portal.get_tool("portal_quickinstaller")
         roles_before = api.user.get_roles(TEST_USER_ID)
-        setRoles(self.portal, TEST_USER_ID, ['Manager'])
-        self.installer.uninstallProducts(['collective.volto.formsupport'])
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+        self.installer.uninstallProducts(["collective.volto.formsupport"])
         setRoles(self.portal, TEST_USER_ID, roles_before)
 
     def test_product_uninstalled(self):
         """Test if collective.volto.formsupport is cleanly uninstalled."""
-        self.assertFalse(self.installer.isProductInstalled(
-            'collective.volto.formsupport'))
+        self.assertFalse(
+            self.installer.isProductInstalled("collective.volto.formsupport")
+        )
 
     def test_browserlayer_removed(self):
         """Test that ICollectiveVoltoFormsupportLayer is removed."""
-        from collective.volto.formsupport.interfaces import \
-            ICollectiveVoltoFormsupportLayer
-        from plone.browserlayer import utils
-        self.assertNotIn(
+        from collective.volto.formsupport.interfaces import (
             ICollectiveVoltoFormsupportLayer,
-            utils.registered_layers())
+        )
+        from plone.browserlayer import utils
+
+        self.assertNotIn(
+            ICollectiveVoltoFormsupportLayer, utils.registered_layers()
+        )
