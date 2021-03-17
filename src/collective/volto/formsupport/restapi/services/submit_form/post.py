eb11db7dc6c92b2703a6ed6749a8d17d56d726d7
Andrea Cecchi <andrea.cecchi85@gmail.com>
2021-03-17 19:41:55 +0100

    fix post

--- src/collective/volto/formsupport/restapi/services/submit_form/post.py
+++ src/collective/volto/formsupport/restapi/services/submit_form/post.py
@@ -19,46 +19,50 @@ import six
 
 
 class SubmitPost(Service):
-    def reply(self):
-        form_data = json_body(self.request)
-        self.validate_form(form_data=form_data)
+    def __init__(self, context, request):
+        super(SubmitPost, self).__init__(context, request)
 
-        block_id = form_data.get("block_id", "")
-        block = self.get_block_data(block_id=block_id)
-        store_action = block.get("store", False)
-        send_action = block.get("send", False)
+        self.block = {}
+        self.form_data = json_body(self.request)
+        self.block_id = self.form_data.get("block_id", "")
+        if self.block_id:
+            self.block = self.get_block_data(block_id=self.block_id)
+
+    def reply(self):
+        self.validate_form()
+
+        store_action = self.block.get("store", False)
+        send_action = self.block.get("send", False)
 
         # Disable CSRF protection
         alsoProvides(self.request, IDisableCSRFProtection)
 
         if store_action:
-            self.store_data(form=form_data)
+            self.store_data()
         if send_action:
-            self.send_data(form=form_data, block=block, block_id=block_id)
+            self.send_data()
 
         return self.reply_no_content()
 
-    def validate_form(self, form_data):
+    def validate_form(self):
         """
         check all required fields and parameters
         """
-        block_id = form_data.get("block_id", "")
-        if not block_id:
+        if not self.block_id:
             raise BadRequest(
                 translate(
                     _("missing_blockid_label", default="Missing block_id"),
                     context=self.request,
                 )
             )
-        block = self.get_block_data(block_id=block_id)
-        if not block:
+        if not self.block:
             raise BadRequest(
                 translate(
                     _(
                         "block_form_not_found_label",
                         default='Block with @type "form" and id "$block" not found in this context: $context',
                         mapping={
-                            "block": block_id,
+                            "block": self.block_id,
                             "context": self.context.absolute_url(),
                         },
                     ),
@@ -66,7 +70,9 @@ class SubmitPost(Service):
                 ),
             )
 
-        if not block.get("store", False) and not block.get("send", False):
+        if not self.block.get("store", False) and not self.block.get(
+            "send", False
+        ):
             raise BadRequest(
                 translate(
                     _(
@@ -77,7 +83,7 @@ class SubmitPost(Service):
                 )
             )
 
-        if not form_data.get("data", []):
+        if not self.form_data.get("data", []):
             raise BadRequest(
                 translate(
                     _("empty_form_data", default="Empty form data.",),
@@ -98,9 +104,13 @@ class SubmitPost(Service):
             return block
         return {}
 
-    def send_data(self, form, block, block_id):
-        subject = form.get("subject", "") or block.get("default_subject", "")
-        mfrom = form.get("from", "") or block.get("default_from", "")
+    def send_data(self):
+        subject = self.form_data.get("subject", "") or self.block.get(
+            "default_subject", ""
+        )
+        mfrom = self.form_data.get("from", "") or self.block.get(
+            "default_from", ""
+        )
         if not subject or not mfrom:
             raise BadRequest(
                 translate(
@@ -121,9 +131,9 @@ class SubmitPost(Service):
 
         registry = getUtility(IRegistry)
         mail_settings = registry.forInterface(IMailSchema, prefix="plone")
-        mto = block.get("to", mail_settings.email_from_address)
+        mto = self.block.get("default_to", mail_settings.email_from_address)
         encoding = registry.get("plone.email_charset", "utf-8")
-        message = self.prepare_message(form=form)
+        message = self.prepare_message()
 
         msg = EmailMessage()
         msg.set_content(message)
@@ -131,27 +141,44 @@ class SubmitPost(Service):
         msg["From"] = mfrom
         msg["To"] = mto
         msg["Reply-To"] = mfrom
+        msg.replace_header("Content-Type", 'text/html; charset="utf-8"')
 
-        self.manage_attachments(form=form, msg=msg)
+        self.manage_attachments(msg=msg)
 
         self.send_mail(msg=msg, encoding=encoding)
 
-    def prepare_message(self, form):
+    def prepare_message(self):
         message_template = api.content.get_view(
             name="send_mail_template",
             context=self.context,
             request=self.request,
         )
         parameters = {
-            "parameters": form.get("data"),
+            "parameters": self.filter_parameters(),
             "url": self.context.absolute_url(),
             "title": self.context.Title(),
         }
         return message_template(**parameters)
 
+    def filter_parameters(self):
+        """
+        do not send attachments fields.
+        """
+        skip_fields = [
+            x.get("field_id", "")
+            for x in self.block.get("subblocks", [])
+            if x.get("field_type", "") == "attachment"
+        ]
+        return [
+            x
+            for x in self.form_data.get("data", [])
+            if x.get("field_id", "") not in skip_fields
+        ]
+
     def send_mail(self, msg, encoding):
         host = api.portal.get_tool(name="MailHost")
         # try:
+
         host.send(msg, charset=encoding)
 
         # except (SMTPException, RuntimeError):
@@ -164,8 +191,8 @@ class SubmitPost(Service):
         #         error=dict(type="InternalServerError", message=message)
         #     )
 
-    def manage_attachments(self, form, msg):
-        attachments = form.get("attachments", {})
+    def manage_attachments(self, msg):
+        attachments = self.form_data.get("attachments", {})
         if not attachments:
             return []
         for key, value in attachments.items():
@@ -194,9 +221,8 @@ class SubmitPost(Service):
                     filename=filename,
                 )
 
-    def store_data(self, form):
-
+    def store_data(self):
         store = getMultiAdapter((self.context, self.request), IFormDataStore)
-        res = store.add(data=form.get("data", {}))
+        res = store.add(data=self.filter_parameters())
         if not res:
             raise BadRequest("Unable to store data")
