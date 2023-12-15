80e3efa2db60cfdefb3df8f869d7d9064135f108
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2023-12-15 13:37:51 +0000

    lint - check-python-versions

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.6",
+    python_requires=">3.6, <3.10",
     install_requires=[
         "setuptools",
         "z3c.jbot",
