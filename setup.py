ec44b0e7a7e29d3c630ec0b0da3a4e0462ba7b4d
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2023-12-15 13:34:42 +0000

    lint - pyroma

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Development Status :: 5 - Production/Stable",
     ],
     keywords="Python Plone CMS",
     author="RedTurtle Technology",
