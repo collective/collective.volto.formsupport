bae6b5597c69994eb88580bf0ce75c5d99d39af7
Andrea Cecchi <andrea.cecchi85@gmail.com>
2024-04-18 10:02:39 +0200

    Back to development: 3.0.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.volto.formsupport",
-    version="3.0.0",
+    version="3.0.1.dev0",
     description="Add support for customizable forms in Volto",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
