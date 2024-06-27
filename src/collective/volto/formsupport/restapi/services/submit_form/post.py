fe46fc4accef30b29bfb0bf408846bbcec200b61
Roman <72063601+folix-01@users.noreply.github.com>
2024-06-27 15:23:48 +0200

    Update post.py

--- src/collective/volto/formsupport/restapi/services/submit_form/post.py
+++ src/collective/volto/formsupport/restapi/services/submit_form/post.py
@@ -277,8 +277,8 @@ class SubmitPost(Service):
             "parameters": self.filter_parameters(),
             "url": self.context.absolute_url(),
             "title": self.context.Title(),
-            "mail_header": self.block.get("mail_header", None),
-            "mail_footer": self.block.get("mail_footer", None),
+            "mail_header": self.block.get("mail_header", {}).get('data', None),
+            "mail_footer": self.block.get("mail_footer", {}).get('data', None),
         }
         return message_template(**parameters)
 
