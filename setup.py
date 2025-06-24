f007b268500b3f1871071d590b90a2e8e7d48925
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-06-25 01:39:54 +0200

    Fixed the 'setup.py says: 3.8, 3.9, 3.10, 3.11 and the python_requires says: 3.8, 3.9, 3.10, 3.11, 3.12' message

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
